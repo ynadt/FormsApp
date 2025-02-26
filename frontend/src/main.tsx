3e3bc7a80bb2ee6d283e09a4fbd437b3903bab17
ynadt <2769286@gmail.com>
2025-02-26 21:32:50 +0300

    feat: remove duplicated queryClient

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './i18n';
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>,
 );
